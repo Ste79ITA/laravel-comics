e0385379c766cf62ee3932c07da87059f7eb5f57
Ste79ITA <nastri.s@gmail.com>
2023-12-11 18:08:35 +0100

    Added layout page with header and footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="page-footer" style="background-image: url({{ Vite::asset('resources/img/footer-bg.jpg') }})">
+    <div class="container">
+    <h1>FOOTER</h1>
+    </div>
+</footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,16 @@
+<header class="page-header">
+<nav class="container">
+    <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="DC Logo">
+
+    <ul class="menu">
+        <li class="menu-item">
+            <a href=""></a>
+            Characters
+        </li>
+        <li class="menu-item">
+            <a href=""></a>
+            Comics
+        </li>
+    </ul>
+</nav>
+</header>
